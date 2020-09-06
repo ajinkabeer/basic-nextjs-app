b50943d935923afd1d86b2dcb9d29fba7f7db2fd
Ajin Kabeer <ajiinkabeer@gmail.com>
2020-09-06 08:12:15 +0530

    Add basic gql boilerplate for playground

--- pages/api/graphql.ts
+++ pages/api/graphql.ts
@@ -1,6 +1,7 @@
 import { ApolloServer } from "apollo-server-micro";
+import { schema } from "src/schema";
 
-const server = new ApolloServer({});
+const server = new ApolloServer({ schema });
 const handler = server.createHandler({ path: "/api/graphql" });
 
 export default handler;
